b9a76b098ae9489d5fccda1580c37900b3f4676f
imkazka <158535796+imkazka@users.noreply.github.com>
2025-02-24 11:04:39 +0700

    Delete acara 5_6 directory

--- acara 5_6/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-
-// Pastikan UserController memiliki method index()
-Route::get('/user', [UserController::class, 'index']);
